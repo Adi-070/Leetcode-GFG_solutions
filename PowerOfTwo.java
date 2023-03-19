fdffbac8563826c01d85ab03c678d2ef1659fa88
Aditya Mishra <mishraaditya760@gmail.com>
2023-03-20 01:53:33 +0530

    Create PowerOfTwo.java

---
+++ PowerOfTwo.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+
+        if(n<=0) return false;
+        while(n%2==0){
+            n/=2;
+        }
+       if (n==1) return true;
+       else return false;
+    }
+}
