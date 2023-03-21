bd6776042bf95d46f6472c67bc027d4291b8d9bb
Aditya Mishra <mishraaditya760@gmail.com>
2023-03-22 04:06:48 +0530

    Update Intersection_twoLinkedlists.java

--- Intersection_twoLinkedlists.java
+++ Intersection_twoLinkedlists.java
@@ -18,3 +18,28 @@ public class Solution {
        b = b.next;   
     }
     
+.........................................................................................................................
+   
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode temp1 = headA;
+        ListNode temp2 = headB;
+        while(temp1!=temp2)
+        {
+            if(temp1==null)
+            temp1=headB;
+            else
+            temp1=temp1.next;
+
+            if(temp2==null)
+            temp2=headA;
+            else
+            temp2=temp2.next;
+        }
+        return temp1;
+
+
+
+        
+    }
+}
