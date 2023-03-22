af02d71322563bc2cd995436fe0fa4c64c4bf214
Aditya Mishra <mishraaditya760@gmail.com>
2023-03-23 01:19:57 +0530

    Create RemoveNodes.java

---
+++ RemoveNodes.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+        public ListNode removeNodes(ListNode head) {
+        if (head == null) 
+        return null;
+
+        head.next = removeNodes(head.next);
+
+        if(head.next != null && head.val < head.next.val)
+        return head.next;
+        else
+        return head;
+    }
+}
