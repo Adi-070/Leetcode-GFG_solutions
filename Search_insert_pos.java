b994a8db5a869c58e0e8ff398931e0cabb4855fa
Aditya Mishra <mishraaditya760@gmail.com>
2023-03-27 02:04:52 +0530

    Update Search_insert_pos.java

--- Search_insert_pos.java
+++ Search_insert_pos.java
@@ -19,3 +19,27 @@ class Solution {
         return index;
     }
 }
+
+...........................................................................
+    
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int low = 0, high = nums.length-1;
+
+        
+
+        while(low<=high)
+        {   int mid=(low+high)/2;
+            if (target==nums[mid])
+            return mid;
+
+            else if(target<nums[mid])
+            high=mid-1;
+
+            else 
+            low=mid+1;
+        }
+        return low;
+    }
+    
+}
