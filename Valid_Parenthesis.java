51a40aa3e53054c87561ad12495f9799302efbc2
Aditya Mishra <mishraaditya760@gmail.com>
2023-03-28 17:51:19 +0530

    Update Valid_Parenthesis.java

--- Valid_Parenthesis.java
+++ Valid_Parenthesis.java
@@ -16,3 +16,35 @@ class Solution {
         return f.isEmpty();
     }
 }
+.................................................................................
+    
+    class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> f = new Stack<Character>();
+        for( char ch : s.toCharArray())
+        {
+            if(ch=='(' || ch=='{' || ch=='[')
+            f.push(ch);
+
+            else if (ch==')')
+            {
+                if( f.isEmpty() || f.pop()!='(' )
+                return false;
+            }
+
+            else if (ch=='}')
+            {
+                if( f.isEmpty()||f.pop()!='{' )
+                return false;
+            }
+
+            else if (ch==']')
+            {
+                if( f.isEmpty()||f.pop()!='['  )
+                return false;
+            }
+
+        }
+        return f.isEmpty();
+}
+}
