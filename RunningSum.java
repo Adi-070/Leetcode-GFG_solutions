6597d0914e9c091f9aedf923a5f0f60e48403e7f
Aditya Mishra <mishraaditya760@gmail.com>
2023-03-30 15:37:20 +0530

    Create RunningSum.java

---
+++ RunningSum.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int[] runningSum = new int[nums.length];
+        int sum = 0;
+
+        for(int i =0;i<nums.length;i++)
+        {
+            sum = sum + nums[i];
+            runningSum[i] = sum;
+        }
+        return runningSum;
+    }
+}
