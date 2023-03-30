dc681f7c91ee04e3315d1b46f0d8365d188ce57c
Aditya Mishra <mishraaditya760@gmail.com>
2023-03-30 20:07:47 +0530

    Create GreatestNumber_of_candies.java

---
+++ GreatestNumber_of_candies.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+
+        ArrayList<Boolean> list = new ArrayList<>();
+        int max = Integer.MIN_VALUE;
+
+        for(int i =0; i<candies.length; i++)
+        {
+            if (candies[i]>max)
+            max = candies[i];
+        }
+          for(int i =0; i<candies.length; i++)
+        {
+            if(candies[i]+extraCandies>=max)
+            list.add(true);
+            else
+            list.add(false);
+        }
+        return list;
+}
+}
