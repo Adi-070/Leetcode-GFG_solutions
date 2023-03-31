1f5c7bbf5e3abd8b91ad69e47469700ddb14f4b1
Aditya Mishra <mishraaditya760@gmail.com>
2023-04-01 01:24:39 +0530

    Update Add_number_Linkedlist.java

--- Add_number_Linkedlist.java
+++ Add_number_Linkedlist.java
@@ -8,6 +8,7 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode(0);
