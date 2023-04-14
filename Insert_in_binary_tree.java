be91f2b491cd2d64265e7d4953a0318b0af5d717
Aditya Mishra <mishraaditya760@gmail.com>
2023-04-14 21:30:47 +0530

    Create Insert_in_binary_tree.java

---
+++ Insert_in_binary_tree.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        TreeNode tree = new TreeNode(val);
+
+        if(root==null)
+        return tree;
+
+        if(val<root.val )
+        root.left = insertIntoBST(root.left,val);
+
+        if(val>root.val )
+        root.right = insertIntoBST(root.right,val);
+
+        return root;
+
+    }
+}
