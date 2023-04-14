002cd61de303fed1b24974771495d72db9aca494
Aditya Mishra <mishraaditya760@gmail.com>
2023-04-14 22:08:58 +0530

    Create Range_sum_BST.java

---
+++ Range_sum_BST.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+     int sum = 0;
+    public int rangeSumBST(TreeNode root, int low, int high) {
+
+            helper(root, low, high);
+            return sum;
+
+    }
+    public void helper(TreeNode root, int low, int high)
+    {
+        if(root==null)
+        return;
+
+        if(root.val>=low && root.val<=high)
+        sum = sum + root.val;
+
+        helper(root.left, low, high);
+        helper(root.right, low, high);
+    }
+}
