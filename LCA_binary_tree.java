ad7d36d78054dc8d3ce90af009d9487b6cae81c7
Aditya Mishra <mishraaditya760@gmail.com>
2023-04-16 13:29:16 +0530

    Create LCA_binary_tree.java

---
+++ LCA_binary_tree.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root==null||root.val==p.val||root.val==q.val)
+        return root;
+
+        TreeNode right = lowestCommonAncestor(root.right,p,q);
+        TreeNode left = lowestCommonAncestor(root.left,p,q);
+
+        if(right==null) return left;
+        else if(left==null) return right;
+
+        return root;
+    }
+}
