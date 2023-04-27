db919e383f6d13d0006f2f6eee5be311c67aa3e5
Aditya Mishra <mishraaditya760@gmail.com>
2023-04-27 16:48:26 +0530

    Added solution - LeetHub

--- Minimum Depth of a Binary Tree - GFG/minimum-depth-of-a-binary-tree.java
+++ Minimum Depth of a Binary Tree - GFG/minimum-depth-of-a-binary-tree.java
@@ -129,8 +129,6 @@ class Solution
 	    if(root==null)
 	    return 0;
 	    
-	   // if(root.left==null && root.right==null)
-	   // return 1;
 	    
 	    int left = minDepth(root.left);
 	    int right = minDepth(root.right);
