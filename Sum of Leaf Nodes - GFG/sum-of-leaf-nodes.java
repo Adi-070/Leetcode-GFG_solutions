f4bf3cdbd6633704aaedf8d83ea6727894ae3338
Aditya Mishra <mishraaditya760@gmail.com>
2023-04-27 16:53:07 +0530

    Added solution - LeetHub

---
+++ Sum of Leaf Nodes - GFG/sum-of-leaf-nodes.java
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+import java.util.Scanner;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Node
+{
+    int data;
+    Node left, right;
+    
+    Node(int key)
+    {
+        data = key;
+        left = right = null;
+    }
+}
+
+class LeafNodes
+{
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            HashMap<Integer, Node> m = new HashMap<Integer, Node>();
+            int n = sc.nextInt();
+            Node root = null;
+            while(n-- > 0)
+            {
+                	int n1 = sc.nextInt();
+                int n2 = sc.nextInt();
+	        	char lr= sc.next().charAt(0);
+				
+                //  cout << n1 << " " << n2 << " " << (char)lr << endl;
+                Node parent = m.get(n1);
+                if (parent == null)
+                {
+                    parent = new Node(n1);
+                    m.put(n1, parent);
+                    if (root == null)
+                        root = parent;
+                }
+                Node child = new Node(n2);
+                if (lr == 'L')
+                    parent.left = child;
+                else
+                    parent.right = child;
+                m.put(n2, child);
+            }
+            
+            GfG gfg = new GfG();
+            System.out.println(gfg.SumofLeafNodes(root));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class GfG
+{
+    public int SumofLeafNodes(Node node)
+    { int sum =0;
+        {   
+         if (node==null)
+         return 0;
+         
+         if (node.left==null && node.right==null)
+         {
+             sum += node.data;
+             return sum;
+         }
+         
+         return SumofLeafNodes(node.left)+SumofLeafNodes(node.right);
+    }
+    }
+}
