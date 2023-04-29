f647f0067aec96587d8427b5816ad1b26c2dc301
Aditya Mishra <mishraaditya760@gmail.com>
2023-04-29 22:46:31 +0530

    Added solution - LeetHub

---
+++ Kadane's Algorithm - GFG/kadanes-algorithm.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+import java.io.*;
+
+import java.util.*;
+
+class Main {
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		while(t-->0){
+		    //size of array
+		    int n = Integer.parseInt(br.readLine().trim());
+		    int arr[] = new int[n];
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    
+		    //adding elements
+		    for(int i=0; i<n; i++){
+		        arr[i] = Integer.parseInt(inputLine[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    
+		    //calling maxSubarraySum() function
+		    System.out.println(obj.maxSubarraySum(arr, n));
+		}
+	}
+}
+
+
+// } Driver Code Ends
+
+
+class Solution{
+
+    int sum=0;
+    int max=Integer.MIN_VALUE;
+    
+    long maxSubarraySum(int arr[], int n){
+        
+        for(int i=0; i<n; i++)
+        {
+            sum += arr[i];
+            
+            max = Math.max(max,sum);
+            
+        if(sum<0)
+        sum=0;
+        }
+        
+        
+        
+        return max;
+        
+    }
+    
+}
+
