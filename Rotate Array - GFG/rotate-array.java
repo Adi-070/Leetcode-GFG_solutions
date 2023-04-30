e5fa776c021982a1a82f2a1ad7593b3a86142392
Aditya Mishra <mishraaditya760@gmail.com>
2023-05-01 01:06:41 +0530

    Added solution - LeetHub

---
+++ Rotate Array - GFG/rotate-array.java
@@ -0,0 +1,79 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+        PrintWriter out=new PrintWriter(System.out);
+        //taking testcases
+        int t = Integer.parseInt(br.readLine()); 
+    	for(int i=0;i<t;i++)
+    	{
+            String str=br.readLine();
+    		String[] starr=str.split(" ");
+    		
+    		//input n and d
+    	    int n=Integer.parseInt(starr[0]);
+    		int d= Integer.parseInt(starr[1]);
+    		
+            int[] arr=new int[n];
+    		String str1=br.readLine();
+    		String[] starr1=str1.split(" ");
+    		
+    		//inserting elements in the array
+    		for(int j=0;j<n;j++)
+    		{
+    		  arr[j]= Integer.parseInt(starr1[j]);
+    		}
+    		
+    		//calling rotateArr() function
+            new Solution().rotateArr(arr, d, n);
+    		StringBuffer sb=new StringBuffer();
+    		
+    		//printing the elements of the array
+            for(int t1=0;t1<n;t1++)
+                sb.append(arr[t1]+" ");
+            out.println(sb);
+         }
+         out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to rotate an array by d elements in counter-clockwise direction. 
+    static void rotateArr(int nums[], int k, int n)
+    {
+        
+         k=(k)%nums.length;
+        
+        reverse(nums,k,nums.length-1);
+        reverse(nums,0,k-1);
+        reverse(nums,0,nums.length-1);
+        
+        
+    }
+    static void reverse(int[] nums, int low, int high)
+    {
+        while(low<high)
+        {
+            int temp=nums[low];
+            nums[low]=nums[high];
+            nums[high]=temp;
+
+            low++;
+            high--;
+        }
+    }
+}
