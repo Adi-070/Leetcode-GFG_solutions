ecaa95d5179563a0b3bf175a8b56a970f40a0fb3
Aditya Mishra <mishraaditya760@gmail.com>
2023-05-06 12:38:50 +0530

    Added solution - LeetHub

---
+++ Intersection of two arrays - GFG/intersection-of-two-arrays.java
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class GFG {
+    public static void main(String[] args) {
+
+        // Taking input using class Scanner
+        Scanner sc = new Scanner(System.in);
+
+        // Taking count of total testcases
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n, m;
+
+            // taking count of elements in array a
+            n = sc.nextInt();
+
+            // taking count of elements in array b
+            m = sc.nextInt();
+
+            // Creating 2 arrays of n and m
+            int a[] = new int[n];
+            int b[] = new int[m];
+
+            // inserting elements to array a
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+
+            // inserting elements to array b
+            for (int i = 0; i < m; i++) {
+                b[i] = sc.nextInt();
+            }
+            Solution ob = new Solution();
+            // calling NumberofElementsInIntersection method
+            // and printing the result
+            System.out.println(ob.NumberofElementsInIntersection(a, b, n, m));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Function to return the count of the number of elements in
+    // the intersection of two arrays.
+    public static int NumberofElementsInIntersection(int a[], int b[], int n, int m) {
+        
+        
+        HashSet<Integer> set1 = new HashSet<Integer>();
+        
+        for(Integer i: a)
+        set1.add(i);
+        
+        HashSet<Integer> set2 = new HashSet<Integer>();
+        
+        for(Integer j: b)
+        set2.add(j);
+        
+        set1.retainAll(set2);
+        
+        return set1.size();
+    }
+};
