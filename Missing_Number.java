6f69c1960d795af92a7fc63cdedc8376774c7bb3
Aditya Mishra <mishraaditya760@gmail.com>
2023-05-07 13:49:05 +0530

    Create Missing_Number.java

---
+++ Missing_Number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+
+        int n = nums.length;
+        int sum = (n*(n+1))/2;
+        
+        int total =0;
+        
+        for(int i=0; i<n; i++)
+        total += nums[i];
+        
+        return sum-total;
+    }
+}
