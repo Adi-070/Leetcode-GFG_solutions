ea6f53eec5edf2cf897f2023d18c59238f35b438
Aditya Mishra <mishraaditya760@gmail.com>
2023-05-09 19:15:22 +0530

    Update Kth_smallest_element_in_sorted_matrix.java

--- Kth_smallest_element_in_sorted_matrix.java
+++ Kth_smallest_element_in_sorted_matrix.java
@@ -15,3 +15,25 @@ class Solution {
         return list.get(k-1);
     }
 }
+
+....................................................................................
+    
+ class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a,b)->Integer.compare(b,a));
+
+        for(int i=0; i<matrix.length; i++)
+        {
+            for(int j=0; j<matrix[0].length; j++)
+            {
+                maxHeap.add(matrix[i][j]);
+
+                if(maxHeap.size()>k)
+                maxHeap.remove();
+            }
+        }
+
+    return maxHeap.peek();
+    }
+}
