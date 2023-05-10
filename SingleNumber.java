57e4ab63a646bb5bebba81383f27eeece4abf9cf
Aditya Mishra <mishraaditya760@gmail.com>
2023-05-10 21:52:51 +0530

    Update SingleNumber.java

--- SingleNumber.java
+++ SingleNumber.java
@@ -11,3 +11,21 @@ class Solution {
     return nums[nums.length-1];
     }
 }
+.................................................................
+    
+    class Solution {
+    public int singleNumber(int[] nums) {
+ HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int i=0;i<nums.length;i++){
+
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+         for(int i=0;i<nums.length;i++){
+            if(map.get(nums[i])==1)
+            return nums[i];
+         }
+        
+        return -1;
+    }
+}
