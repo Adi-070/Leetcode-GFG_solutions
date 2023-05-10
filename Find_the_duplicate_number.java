b532775df539bef19d5751e0e85095af1e0a0a03
Aditya Mishra <mishraaditya760@gmail.com>
2023-05-10 15:57:32 +0530

    Update Find_the_duplicate_number.java

--- Find_the_duplicate_number.java
+++ Find_the_duplicate_number.java
@@ -12,3 +12,21 @@ class Solution {
         return -1;
     }
 }
+.............................................................................
+    
+  class Solution {
+    public int findDuplicate(int[] nums) {
+        
+        HashMap<Integer,Integer> map = new HashMap<>();
+        
+        for(int i=0;i<nums.length;i++){
+            
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+            
+            if(map.get(nums[i])==2)
+            return nums[i];
+            
+        }
+        return -1;
+    }
+}
