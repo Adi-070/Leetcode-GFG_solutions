16f4f0447c5beaa4e3b792e18e83c33a3efe1859
Aditya Mishra <mishraaditya760@gmail.com>
2023-05-10 13:56:58 +0530

    Create Search_in_a_2D_matrix.java

---
+++ Search_in_a_2D_matrix.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+
+        // if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+        //     return false;
+        // }
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int i=0;
+        int j=matrix[0].length-1;
+
+        while(i<m && j>=0)
+        {
+            if(matrix[i][j]==target)
+            return true;
+
+            else if(matrix[i][j]>target)
+            j--;
+
+            else
+            i++;
+        }
+        return false;
+    }
+}
