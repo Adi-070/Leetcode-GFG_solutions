37b6cd6d692d1630f08fb6b1d15447fd8820b8fa
Aditya Mishra <mishraaditya760@gmail.com>
2023-05-12 03:09:38 +0530

    Added solution - LeetHub

--- Array Subset of another array - GFG/array-subset-of-another-array.java
+++ Array Subset of another array - GFG/array-subset-of-another-array.java
@@ -48,34 +48,19 @@ class GFG {
 class Compute {
     public String isSubset( long a1[], long a2[], long n, long m) {
         
-        HashMap<Long, Integer> map = new HashMap<>();
+        HashMap<Long,Integer> map = new HashMap<>();
         
         for(long i:a1)
-        {
-            if (map.containsKey(i))
-            map.put(i,map.get(i)+1);
-            
-            else
-            map.put(i,1);
-        }
+        map.put(i,map.getOrDefault(i,0)+1);
         
         for(long i:a2)
         {
-            if(map.containsKey(i))
-            {
-                if(map.get(i)==1)
-                map.remove(i);
+            if(!map.containsKey(i)||map.get(i)==0)
+            return "No";
             
             else
             map.put(i,map.get(i)-1);
-            
-            }
-            
-            else 
-            return "No";
         }
-        
         return "Yes";
-        
     }
 }
