1bf3ade69ca711d9831bbb66fcf4b2f9b0b0de5a
Aditya Mishra <mishraaditya760@gmail.com>
2023-05-12 03:43:15 +0530

    Added solution - LeetHub

---
+++ Common elements - GFG/common-elements.java
@@ -0,0 +1,86 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+
+public class GFG
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) 
+        {
+            int n1 = sc.nextInt();
+            int n2 = sc.nextInt();
+            int n3 = sc.nextInt();
+            
+            int A[] = new int[n1];
+            int B[] = new int[n2];
+            int C[] = new int[n3];
+            
+            for (int i = 0;i < n1;i++)
+            {
+                A[i] = sc.nextInt();
+            }
+            for (int i = 0;i < n2;i++)
+            {
+                B[i] = sc.nextInt();
+            }
+            for (int i = 0;i < n3;i++)
+            {
+                C[i] = sc.nextInt();
+            }
+            
+            Solution sol = new Solution();
+            ArrayList<Integer> res = sol.commonElements(A, B, C, n1, n2, n3);
+            if (res.size() == 0)
+            {
+                System.out.print(-1);
+            }
+            else 
+            {
+                for (int i = 0;i < res.size();i++)
+                {
+                    System.out.print(res.get(i) + " ");
+                }    
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    ArrayList<Integer> commonElements(int A[], int B[], int C[], int n1, int n2, int n3) 
+    {
+        ArrayList<Integer> list = new ArrayList<>(); 
+        HashSet<Integer> set1 = new HashSet<>();
+        HashSet<Integer> set2 = new HashSet<>();
+        
+        for(int i:A)
+        set1.add(i);
+        
+        for(int i:B)
+        {
+            if(set1.contains(i))
+            set2.add(i);
+        }
+        
+        for(int i:C)
+        {
+            if(set2.contains(i))
+            {
+                if(!list.contains(i))
+                list.add(i);
+            }
+        }
+        return list;
+    }
+}
