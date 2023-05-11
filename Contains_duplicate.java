79fb67ef2027b71436654831a63c7f7e9e61ddf1
Aditya Mishra <mishraaditya760@gmail.com>
2023-05-11 23:38:20 +0530

    Update Contains_duplicate.java

--- Contains_duplicate.java
+++ Contains_duplicate.java
@@ -12,3 +12,21 @@ class Solution {
         return false;
     }
 }
+.........................................
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int i:nums)
+        {
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+
+        for(int i:nums)
+        {
+            if(map.get(i)>1)
+            return true;
+        }
+        return false;
+    }
+}
