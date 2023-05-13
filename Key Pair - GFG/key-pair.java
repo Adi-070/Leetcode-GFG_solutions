41624b562655dab1019a51c8ad8a5067791735d5
Aditya Mishra <mishraaditya760@gmail.com>
2023-05-13 17:55:55 +0530

    Added solution - LeetHub

---
+++ Key Pair - GFG/key-pair.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int x = Integer.parseInt(inputLine[1]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            boolean ans = new Solution().hasArrayTwoCandidates(arr, n, x);
+            System.out.println(ans ? "Yes" : "No");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+    boolean hasArrayTwoCandidates(int arr[], int n, int X) {
+        
+        Arrays.sort(arr);
+        
+        int i=0;
+        
+        int j=n-1;
+        
+        while(i<j)
+        {
+            if(arr[i]+arr[j]==X)
+            return true;
+            
+            else if(arr[i]+arr[j]<X)
+            i++;
+            
+            else
+            j--;
+        }
+        return false;
+    }
+}
