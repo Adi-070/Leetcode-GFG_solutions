ad58de3b11d324d6e7aa5a679223a03bee83038f
Aditya Mishra <mishraaditya760@gmail.com>
2023-05-15 22:36:47 +0530

    Update Swapping_nodes_in_linkedlist.java

--- Swapping_nodes_in_linkedlist.java
+++ Swapping_nodes_in_linkedlist.java
@@ -8,17 +8,26 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode swapNodes(ListNode head, int k) {
-        int c=0;
+        int c=1;
 
         ListNode temp = head;
 
         while(temp.next!=null)
         {   
-             c++;
-            temp=temp.next;
-           
+        temp=temp.next;
+           c++;
         }
 
         ListNode first=head;
@@ -28,7 +37,7 @@ class Solution {
         {
             first=first.next;
         }
-        for(int i=0; i<=c-k;i++)
+        for(int i=0; i<c-k;i++)
         {
             second=second.next;
         }
