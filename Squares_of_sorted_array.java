203ef8276a8474f149deba308ee24e914f1356b7
Aditya Mishra <mishraaditya760@gmail.com>
2023-05-15 20:11:20 +0530

    Update Squares_of_sorted_array.java

--- Squares_of_sorted_array.java
+++ Squares_of_sorted_array.java
@@ -4,11 +4,37 @@ class Solution {
        
 
         for(int i=0; i<nums.length; i++)
-        {
+        {                                                                                                       // BRUTE FORCE
             nums[i]=nums[i]*nums[i];
         }
 
         Arrays.sort(nums);
         return nums;
+    }
+}
+
+...........................................................
+    
+    class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int left =0;
+        int right = nums.length-1;
+
+        int result[] = new int[nums.length];
+
+        for(int i=nums.length-1; i>=0; i--)                                                                         // OPTIMISED
+        {
+            if(Math.abs(nums[left])>Math.abs(nums[right]))
+            {
+                result[i]=nums[left]*nums[left];
+                left++;
+            }
+            else
+            {
+                result[i]=nums[right]*nums[right];
+                right--;
+            }
+        }
+        return result;
     }
 }
