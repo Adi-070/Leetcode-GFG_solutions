3f315ceab588638f6fc90f91164ea573e61aa22d
Aditya Mishra <mishraaditya760@gmail.com>
2023-05-15 19:57:17 +0530

    Create Squares_of_sorted_array.java

---
+++ Squares_of_sorted_array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        
+       
+
+        for(int i=0; i<nums.length; i++)
+        {
+            nums[i]=nums[i]*nums[i];
+        }
+
+        Arrays.sort(nums);
+        return nums;
+    }
+}
