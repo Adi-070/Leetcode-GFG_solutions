609508f12b84547d6a8143fc30fe01182d645c67
Aditya Mishra <mishraaditya760@gmail.com>
2023-05-18 15:13:54 +0530

    Create Majority_element_2.java

---
+++ Majority_element_2.java
@@ -0,0 +1,57 @@
+class Solution {
+    public List<Integer> majorityElement(int[] arr) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        int nums1 = Integer.MIN_VALUE;
+        int nums2 = Integer.MIN_VALUE;
+
+        int count1=0;
+        int count2=0;
+
+        for(int i=0; i<arr.length; i++)
+        {
+            if(arr[i]==nums1)
+            {
+                count1++;
+            }
+            else if(arr[i]==nums2)
+            {
+                count2++;
+            }
+            else if(count1==0)
+            {
+                nums1=arr[i];
+                count1=1;
+            }
+            else if(count2==0)
+            {
+                nums2=arr[i];
+                count2=1;
+            }
+            else
+            {
+                count1--;
+                count2--;
+            }
+        }
+        count1=0;
+        count2=0;
+
+        for(int i=0; i<arr.length; i++)
+        {
+            if(arr[i]==nums1)
+            count1++;
+
+            if(arr[i]==nums2)
+            count2++;
+        }
+        if(count1 > arr.length/3)
+        list.add(nums1);
+
+        if(count2>arr.length/3)
+        list.add(nums2);
+
+        return list;
+    }
+
+}
