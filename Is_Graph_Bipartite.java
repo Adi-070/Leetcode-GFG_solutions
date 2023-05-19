24b5daa8b1a01ff601b1cdc7c1d0ab8e0f2566ab
Aditya Mishra <mishraaditya760@gmail.com>
2023-05-19 11:48:52 +0530

    Update Is_Graph_Bipartite.java

--- Is_Graph_Bipartite.java
+++ Is_Graph_Bipartite.java
@@ -1,25 +1,25 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
-        int colors[] = new int[graph.length];
+        int[] colors = new int[graph.length];
 
         for(int i=0; i<graph.length; i++)
         {
             if(colors[i]==0 && !isValid(graph,colors,i,1))
             return false;
+
         }
         return true;
     }
-
-    public boolean isValid(int[][] graph, int[] colors, int node, int curr)
+    public boolean isValid(int[][] graph, int[] colors, int node, int c)
     {
         if(colors[node]!=0)
-        return colors[node]==curr;
+        return colors[node]==c;
 
-        colors[node]=curr;
+        colors[node]=c;
 
-        for(int i: graph[node])
+        for(int i:graph[node])
         {
-            if (!isValid(graph,colors,i,-curr))
+            if(!isValid(graph,colors,i,-c))
             return false;
         }
         return true;
