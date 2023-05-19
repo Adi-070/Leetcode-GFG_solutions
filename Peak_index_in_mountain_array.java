3dc6938b5a1ef4715f32bbcf8a90b7859ae5a8f4
Aditya Mishra <mishraaditya760@gmail.com>
2023-05-19 14:07:59 +0530

    Update Peak_index_in_mountain_array.java

--- Peak_index_in_mountain_array.java
+++ Peak_index_in_mountain_array.java
@@ -9,3 +9,24 @@ class Solution {
         return -1;
     }
 }
+....................................................................
+    
+    class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        
+        int low=0;
+        int high=arr.length-1;
+
+        while(low<=high)
+        {
+            int mid = low + (high-low)/2;
+
+            if(arr[mid]>arr[mid+1])
+            high=mid-1;
+
+            else
+            low=mid+1;
+        }
+        return low;
+    }
+}
