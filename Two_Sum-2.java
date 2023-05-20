beb817b9ddd6f5607a44fb1c78fa8afcf0fa978b
Aditya Mishra <mishraaditya760@gmail.com>
2023-05-20 18:50:02 +0530

    Create Two_Sum-2.java

---
+++ Two_Sum-2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+       
+        int low=0;
+        int high=numbers.length-1;
+
+        while(low<=high)
+        {
+            if(numbers[low]+numbers[high]==target)
+            return new int[]{low+1,high+1};
+
+            if(numbers[low]+numbers[high]<target)
+            low++;
+
+            else
+            high--;
+        }
+        return new int[]{-1};
+    }
+}
