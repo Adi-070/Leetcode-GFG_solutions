a73d88dab3b71d7ae433973cd0e460b964892195
Aditya Mishra <mishraaditya760@gmail.com>
2023-05-21 10:59:07 +0530

    Update Two_Sum_IV.java

--- Two_Sum_IV.java
+++ Two_Sum_IV.java
@@ -13,6 +13,58 @@
  *     }
  * }
  */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean findTarget(TreeNode root, int k) {
+        
+        ArrayList<Integer> list = new ArrayList<>();
+
+        if(root==null)
+        return false;
+
+        inorder(root,list);
+
+        int i=0;
+        int j=list.size()-1;
+
+        while(i<j)
+        {
+            if(list.get(i)+list.get(j)==k)
+            return true;
+
+            else if(list.get(i)+list.get(j)<k)
+            i++;
+
+            else
+            j--;
+        }
+        return false;
+    }
+   public void inorder(TreeNode root, ArrayList<Integer> list)                                           // - O(n)
+    {
+        if(root==null)
+        return;
+
+        inorder(root.left,list);
+        list.add(root.val);
+        inorder(root.right,list);
+    }
+}
+...........................................................................................................
 class Solution {
     public boolean findTarget(TreeNode root, int k) {
         
