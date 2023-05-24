24a7d97c40dae3d0a3d53e7df885da27b4bce739
Aditya Mishra <mishraaditya760@gmail.com>
2023-05-25 01:48:57 +0530

    Update Kth_missing_positive_number.java

--- Kth_missing_positive_number.java
+++ Kth_missing_positive_number.java
@@ -2,7 +2,7 @@ class Solution {
     public int findKthPositive(int[] arr, int k) {
         int left =0;
         int right = arr.length-1;
-
+                                                                                //O(log n)
         while(left<=right)
         {
             int mid = left + (right-left)/2;
@@ -14,5 +14,15 @@ class Solution {
             right=mid-1;
         }
         return left+k;
+    }
+}
+....................................................................
+    
+    class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        for(int i : arr){                                                       //O(n)
+			if(i <= k) k++; else break;
+		}
+        return k;
     }
 }
