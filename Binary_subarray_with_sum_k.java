1d4439228727063468e15aedc1512bbe55e8a242
Aditya Mishra <mishraaditya760@gmail.com>
2023-06-02 22:34:12 +0530

    Update and rename binary_subarray_with_sum_k.java to Binary_subarray_with_sum_k.java

--- Binary_subarray_with_sum_k.java
+++ Binary_subarray_with_sum_k.java
@@ -30,3 +30,29 @@ class Solution {
         return c;
     }
 }
+...................................................................................................................
+    
+    
+    class Solution {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        int sum=0;
+        int c=0;
+
+        map.put(0,1);
+
+        for(int i:nums)
+        {   
+            sum +=  i;
+
+            if(map.containsKey(sum-goal))
+            c += map.get(sum-goal);
+
+            map.put(sum,map.getOrDefault(sum,0)+1);
+
+        }
+        return c;
+    }
+}
