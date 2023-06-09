308e5cb89aeb54e94cd05eced68548e7b3a7b3d6
Aditya Mishra <mishraaditya760@gmail.com>
2023-06-09 19:24:30 +0530

    Update Smallest_number_greater_than_target.java

--- Smallest_number_greater_than_target.java
+++ Smallest_number_greater_than_target.java
@@ -9,3 +9,29 @@ class Solution {
         return letters[0];
     }
 }
+
+...............................................................................
+    
+    
+    class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        
+        int left = 0;
+        int right= letters.length-1;
+
+        while(left<=right)
+        {
+            int mid= left + (right-left)/2;
+
+            if(letters[mid]>target)
+            right=mid-1;
+
+            else
+            left=mid+1;
+        }
+        if(left==letters.length)
+        return letters[0];
+
+        return letters[left];
+    }
+}
