bc1b25dd1c1c8bc4c0991b095b0d7f4a00242cc8
Aditya Mishra <mishraaditya760@gmail.com>
2023-06-11 15:47:04 +0530

    Update Find_difference.java

--- Find_difference.java
+++ Find_difference.java
@@ -28,3 +28,23 @@ class Solution {
         return  'a';
     }
 }
+
+........................................................................................
+    
+class Solution {
+    public char findTheDifference(String s, String t) {
+        
+        char c[] = s.toCharArray();
+        char d[] = t.toCharArray(); 
+
+        Arrays.sort(c);
+        Arrays.sort(d);
+
+        for(int i=0; i<c.length; i++)
+        {
+            if(c[i]!=d[i])
+            return d[i];
+        }
+        return d[d.length-1];
+    }
+}
