a562b847893da8ab284ffd0472f55fda89f54d9c
Aditya Mishra <mishraaditya760@gmail.com>
2023-06-11 15:38:24 +0530

    Create Find_difference.java

---
+++ Find_difference.java
@@ -0,0 +1,30 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        
+        HashMap<Character,Integer> map1 = new HashMap<>();
+        HashMap<Character,Integer> map2 = new HashMap<>();
+
+        for(int i=0; i<s.length(); i++)
+        {
+            map1.put(s.charAt(i),map1.getOrDefault(s.charAt(i),0)+1);
+        }
+        
+         for(int i=0; i<t.length(); i++)
+        {
+            map2.put(t.charAt(i),map2.getOrDefault(t.charAt(i),0)+1);
+        }
+        // System.out.println(map2);
+        // System.out.println(map1);
+        
+        for( char c: map2.keySet())
+        {
+            if(!map1.containsKey(c))
+            return c;
+
+            else if(map1.get(c)<map2.get(c))
+            return c;
+            
+        }
+        return  'a';
+    }
+}
