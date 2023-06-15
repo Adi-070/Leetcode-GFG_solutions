21be876d5f8e128a19e8f4f7fce361a739f4aee8
Aditya Mishra <mishraaditya760@gmail.com>
2023-06-15 22:54:27 +0530

    Create Final_value_of_variable.java

---
+++ Final_value_of_variable.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        
+        int x=0;
+
+        for(int i=0; i<operations.length; i++)
+        {
+            if(operations[i].equals("++X") || operations[i].equals("X++"))
+            x++;
+
+            else
+            x--;
+        }
+        return x;
+    }
+}
