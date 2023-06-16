f0817e160db989bbec50e3315117f301a690c412
Aditya Mishra <mishraaditya760@gmail.com>
2023-06-16 15:30:51 +0530

    Update Valid_BST.java

--- Valid_BST.java
+++ Valid_BST.java
@@ -34,3 +34,41 @@ class Solution {
         return helper(root.left,low,root) && helper(root.right,root,high);
     }
 }
+..................................................................................................
+    
+    
+    /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+       long min = Long.MIN_VALUE;
+        long max = Long.MAX_VALUE;
+        
+        return helper(root,min,max);
+    }
+    
+    boolean helper(TreeNode root, long low, long high)
+    {
+        if(root==null)
+        return true;
+        
+        if(root.val<=low || root.val>=high)
+        return false;
+        
+        
+        return helper(root.left,low,root.val) && helper(root.right,root.val,high);
+    }
+}
