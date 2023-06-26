e765cfa18b3b49cd011a3084847b0a6e5601efd8
Aditya Mishra <mishraaditya760@gmail.com>
2023-06-26 13:22:16 +0530

    Create Search_in_rotated_sorted_array_2.java

---
+++ Search_in_rotated_sorted_array_2.java
@@ -0,0 +1,43 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+          int start =0;
+        int end = nums.length-1;
+    
+    while(start<=end)
+    {
+        int mid = start + (end-start)/2;
+
+        if (nums[mid]==target)
+        return true;
+
+         if(nums[start]==nums[mid] && nums[mid] == nums[end])
+        {
+            start++;
+            end--;
+            continue;
+        }
+
+        if(nums[start]<=nums[mid])
+        {
+            if (target>=nums[start]&& target<=nums[mid])
+            end = mid-1;
+
+            else
+            start=mid+1;
+        }
+
+        else
+        {
+            if (target>=nums[mid]&& target<=nums[end])
+            start=mid+1;
+
+            else
+            end=mid-1;
+        }
+    }
+        return false;
+    }
+
+}
+
+
