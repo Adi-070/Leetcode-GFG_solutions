c287f627e0c5b3830aad5cf4908a060c4cfd500b
Aditya Mishra <mishraaditya760@gmail.com>
2023-06-29 01:46:36 +0530

    Create Valid_substitution.java

---
+++ Valid_substitution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isValid(String s) {
+
+        while(s.contains("abc"))
+        {
+        s=s.replace("abc","");
+        }
+        
+        return s.isEmpty();
+    }
+}
