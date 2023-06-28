7daf13b788355566079e3baca4ce4da11315d7b2
Aditya Mishra <mishraaditya760@gmail.com>
2023-06-29 02:03:34 +0530

    Update Valid_substitution.java

--- Valid_substitution.java
+++ Valid_substitution.java
@@ -9,3 +9,37 @@ class Solution {
         return s.isEmpty();
     }
 }
+................................................................
+
+class Solution {
+    public boolean isValid(String s) {
+
+      Stack<Character> stk = new Stack<>();
+
+      for(int i=0; i<s.length(); i++)
+      {
+          if(s.charAt(i)=='a' || s.charAt(i)=='b')
+          stk.push(s.charAt(i));
+
+          else {
+
+            if(stk.size()>0 && stk.peek()=='b')
+            stk.pop();
+
+            else
+            return false;
+
+            if(stk.size()>0 && stk.peek()=='a')
+            stk.pop();
+
+            else
+            return false;
+          }
+      }  
+
+      if(stk.size()==0)
+      return true;
+
+      return false;
+    }
+}
