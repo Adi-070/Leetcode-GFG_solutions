a3f0714d7d95888f821398cba7de8e5885389fe9
Aditya Mishra <mishraaditya760@gmail.com>
2023-07-24 17:23:41 +0530

    Added solution - LeetHub

---
+++ Insertion Sort - GFG/insertion-sort.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.Math;
+
+class Sorting
+{
+	
+	static void printArray(int arr[],int size)
+	{
+		int i;
+		for(i=0;i<size;i++)
+		System.out.print(arr[i]+" ");
+	    System.out.println();
+	}
+	
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t= sc.nextInt();
+		while(t>0)
+		{
+			int n = sc.nextInt();
+			int a[] = new int[n];
+		
+			for(int i=0;i<n;i++)
+			a[i]= sc.nextInt();
+			
+			 new Solution().insertionSort(a,n);
+			 printArray(a,n);
+			
+		t--;
+		}
+		
+	}
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+  static void insert(int arr[],int i)
+  {
+       // Your code here
+  }
+  //Function to sort the array using insertion sort algorithm.
+  public void insertionSort(int arr[], int n)
+  {
+      for(int i=0; i<n; i++)
+      {
+          int index = i;
+          
+          while(index>0 && arr[index-1]>arr[index])
+          {
+              int temp = arr[index-1];
+              arr[index-1]=arr[index];
+              arr[index]=temp;
+              
+              index--;
+          }
+      }
+  }
+}
