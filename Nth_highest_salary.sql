5c86ec9a5c7cdf300fd643a119cb9bc6afa94bff
Aditya Mishra <mishraaditya760@gmail.com>
2023-08-18 00:21:32 +0530

    Create Nth_highest_salary.sql

---
+++ Nth_highest_salary.sql
@@ -0,0 +1,9 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+
+SET N = N-1;
+  RETURN (
+      # Write your MySQL query statement below.
+      Select distinct salary from Employee order by salary desc limit 1 offset N
+  );
+END
