1194ef583116925f6c081345c17b2dd04118e486
Aditya Mishra <mishraaditya760@gmail.com>
2023-08-19 18:00:27 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -12,7 +12,7 @@ public int fib(int n) {
 
 class Solution {
 public int fib(int n) {
-                                                                                                //MEMOIZATION
+                                     //MEMOIZATION
         int[] dp = new int[n+1];
         Arrays.fill(dp,-1);
 
@@ -23,6 +23,27 @@ public int fib(int n) {
         return dp[n];
 
         return dp[n]=fib(n-1)+fib(n-2);
+    }
+}
+...............................................
+
+
+class Solution {
+public int fib(int n) {
+
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+
+        if(n<=1)
+        return n;
+
+        dp[0]=0;                    //TABULATION
+        dp[1]=1;
+
+        for(int i=2; i<=n; i++)
+        dp[i]=dp[i-1]+dp[i-2];
+
+        return dp[n];
     }
 }
 
