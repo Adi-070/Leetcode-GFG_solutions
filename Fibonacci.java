fc75607f36cb6810e661b3d50862a2743dee5342
Aditya Mishra <mishraaditya760@gmail.com>
2023-08-19 17:37:37 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -6,3 +6,23 @@ public int fib(int n) {
         return fib(n-1)+fib(n-2);
     }
 }
+
+.............................................
+
+
+class Solution {
+public int fib(int n) {
+                                                                                                //MEMOIZATION
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+
+        if(n<=1)
+        return n;
+
+        if(dp[n]!=-1)
+        return dp[n];
+
+        return dp[n]=fib(n-1)+fib(n-2);
+    }
+}
+
