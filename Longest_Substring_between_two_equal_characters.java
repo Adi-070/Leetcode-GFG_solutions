bd6061c145b65178362e3b16975ae3f52b307e3e
Aditya Mishra <mishraaditya760@gmail.com>
2024-01-01 02:46:58 +0530

    Update Longest_Substring_between_two_equal_characters.java

--- Longest_Substring_between_two_equal_characters.java
+++ Longest_Substring_between_two_equal_characters.java
@@ -20,3 +20,21 @@ class Solution {
         return max;
     }
 }
+.........................................................................
+
+class Solution {
+    public int maxLengthBetweenEqualCharacters(String s) {
+        HashMap<Character,Integer> map = new HashMap<>();
+        int max=-1;
+
+        for(int i=0; i<s.length(); i++)
+        {
+            if(map.containsKey(s.charAt(i)))
+            max=Math.max(max,i-map.get(s.charAt(i))-1);
+
+            else
+            map.put(s.charAt(i),i);
+        }
+        return max;
+    }
+}
