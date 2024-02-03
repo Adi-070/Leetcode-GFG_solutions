64bb7c31135b4d39a67b56eed057c02871e0f62f
Aditya Mishra <mishraaditya760@gmail.com>
2024-02-03 22:20:32 +0530

    Update Spiral_matrix.java

--- Spiral_matrix.java
+++ Spiral_matrix.java
@@ -1,3 +1,48 @@
+
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> list = new ArrayList<>();
+
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int top=0; int left=0;
+        int bottom=n-1; int right=m-1;
+
+while(top<=bottom && left<=right)
+{
+        for(int i=left; i<=right; i++)
+        {
+            list.add(matrix[top][i]);
+        }
+        top++;
+
+        for(int i=top; i<=bottom; i++)
+        {
+            list.add(matrix[i][right]);
+        }
+        right--;
+if(top<=bottom)
+{
+        for(int i=right; i>=left; i--)
+        {
+            list.add(matrix[bottom][i]);
+        }
+        bottom--;
+}
+if(left<=right)
+{
+        for(int i=bottom; i>=top; i--)
+        {
+            list.add(matrix[i][left]);
+        }
+        left++;
+}
+}
+return list;
+    }
+}
+.....................................................................................
 class Solution {
     List<Integer> ans = new ArrayList<>();
     public List<Integer> spiralOrder(int[][] matrix) {
