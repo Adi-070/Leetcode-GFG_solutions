83a047a58d65084096c9a6b1156faaeb25c433f1
Aditya Mishra <mishraaditya760@gmail.com>
2024-02-06 19:41:09 +0530

    Update Pascals_triangle.java

--- Pascals_triangle.java
+++ Pascals_triangle.java
@@ -1,5 +1,34 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> list = new ArrayList<>();
+
+        for(int row=1; row<=numRows; row++)
+        {
+            list.add(find(row));
+        }
+    return list;
+    }
+    public List<Integer> find(int rows)
+    {
+        List<Integer> res = new ArrayList<>();
+        res.add(1);
+
+        int ans=1;
+
+        for(int col=1; col<rows; col++)
+        {
+            ans = ans*(rows-col);
+            ans=ans/col;
+
+            res.add(ans);
+        }
+        return res;
+    }
+}
+
+................................................................
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
         
         List<List<Integer>> ans = new ArrayList<>();
 
