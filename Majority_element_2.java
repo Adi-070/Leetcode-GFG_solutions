355d0e39db0807f6c0e82d2222dcefaebff10ec3
Aditya Mishra <mishraaditya760@gmail.com>
2024-02-18 23:49:40 +0530

    Update Majority_element_2.java

--- Majority_element_2.java
+++ Majority_element_2.java
@@ -55,3 +55,61 @@ class Solution {
     }
 
 }
+...........................................................................
+
+
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        
+        List<Integer> list = new ArrayList<>();
+        int n = nums.length;
+        int num1=Integer.MIN_VALUE;
+        int num2=Integer.MIN_VALUE;
+
+        int count1=0;
+        int count2=0;
+
+        for(int i=0; i<nums.length; i++)
+        {
+            if(count1==0 && nums[i]!=num2)
+            {
+                num1=nums[i];
+                count1=1;
+            }
+            else if(count2==0 && nums[i]!=num1)
+            {
+                num2=nums[i];
+                count2=1;
+            }
+            else if(nums[i]==num1)
+            count1++;
+
+            else if(nums[i]==num2)
+            count2++;
+
+            else {
+                count1--;
+                count2--;
+            }
+        }
+        count1=0;
+        count2=0;
+
+        for(int i=0; i<nums.length; i++)
+        {
+            if(nums[i]==num1)
+            count1++;
+
+            if(nums[i]==num2)
+            count2++;
+        }
+
+        if(count1 > n/3)
+        list.add(num1);
+
+        if(count2 > n/3)
+        list.add(num2);
+
+        return list;
+    }
+}
