cf6a0a5ef4faa52e4d28b8209234c6774cf563b8
Aditya Mishra <mishraaditya760@gmail.com>
2024-02-19 11:47:25 +0530

    Update Power_Of_Two.java

--- Power_Of_Two.java
+++ Power_Of_Two.java
@@ -1,5 +1,21 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
+        
+        if(n<=0)
+        return false;
+
+        if(n==1)
+        return true;
+
+        if(n%2!=0)
+        return false;
+
+        return isPowerOfTwo(n/2);
+    }
+}
+......................................................
+class Solution {
+    public boolean isPowerOfTwo(int n) {
 
         if(n<=0) return false;
         while(n%2==0){
