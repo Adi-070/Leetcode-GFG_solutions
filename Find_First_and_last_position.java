b592b46c1db59d6cd14e5cee182a7da4283fe2f4
Aditya Mishra <mishraaditya760@gmail.com>
2024-02-20 19:19:30 +0530

    Update Find_First_and_last_position.java

--- Find_First_and_last_position.java
+++ Find_First_and_last_position.java
@@ -16,3 +16,66 @@ class Solution {
 }
 
 ...............................................................................................
+
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        
+        int res[] = {-1,-1};
+
+        int first = first(nums,target);
+        int last = last(nums,target);
+
+        res[0]=first;
+        res[1]=last;
+
+        return res;
+    }
+    public int first(int[] nums, int target)
+    {
+        int low=0;
+        int high=nums.length-1;
+
+        int res=-1;
+
+        while(low<=high)
+        {   int mid = low + (high-low)/2;
+            if(nums[mid]<target)
+            {
+                low=mid+1;
+            }
+            else if(nums[mid]>target)
+            {
+                high=mid-1;
+            }
+            else {
+                res=mid;
+                high=mid-1;
+            }
+        }
+        return res;
+    }
+    public int last(int[] nums, int target)
+    {
+        int low=0;
+        int high=nums.length-1;
+
+        int res=-1;
+
+        while(low<=high)
+        {   int mid = low + (high-low)/2;
+            if(nums[mid]<target)
+            {                                                                                //OPTIMAL
+                low=mid+1;
+            }
+            else if(nums[mid]>target)
+            {
+                high=mid-1;
+            }
+            else {
+                res=mid;
+                low=mid+1;
+            }
+        }
+        return res;
+    }
+}
