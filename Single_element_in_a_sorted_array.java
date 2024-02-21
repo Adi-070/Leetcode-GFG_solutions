dbde7a023fb49f78273f23515a46837988f8ef78
Aditya Mishra <mishraaditya760@gmail.com>
2024-02-22 00:30:06 +0530

    Update Single_element_in_a_sorted_array.java

--- Single_element_in_a_sorted_array.java
+++ Single_element_in_a_sorted_array.java
@@ -1,38 +1,42 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
         
-        int low =0;
-        int high = nums.length-1;
+        int n = nums.length; 
+
+    //Edge cases:
+    if (n == 1) return nums[0];
+    if (nums[0] != nums[1]) return nums[0];
+    if (nums[n - 1] != nums[n - 2]) return nums[n - 1];
+
+        int low=0;
+        int high=nums.length-1;
 
         int mid=0;
 
         while(low<=high)
         {
-             mid = low + (high-low)/2;
+            mid = low + (high-low)/2;
 
-            int num = mid+1;
-
-            if(num%2==0) {
-
-                if(mid>0 && nums[mid]==nums[mid-1])
-                low=mid+1;
-
-                else if(mid<nums.length-1 && nums[mid]==nums[mid+1])
-                high=mid-1;
-
-                 else 
-                  return nums[mid];
-            }
-
-            else if(num%2!=0)
+            if(mid%2==0)
             {
-                if(mid>0 && nums[mid]==nums[mid-1])
-                high=mid-1;
-
-                else if(mid<nums.length-1 && nums[mid]==nums[mid+1])
+                if(nums[mid]==nums[mid+1])
                 low=mid+1;
 
-                else 
+                else if(nums[mid]==nums[mid-1])
+                high=mid-1;
+
+                else
+                return nums[mid];
+            }
+             if(mid%2!=0)
+            {
+                if(nums[mid]==nums[mid+1])
+                high=mid-1;
+
+                else if(nums[mid]==nums[mid-1])
+                low=mid+1;
+
+                else
                 return nums[mid];
             }
         }
