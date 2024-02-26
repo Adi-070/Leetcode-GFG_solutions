0552505c719a2864fac062d3b28a6794b87bca38
Aditya Mishra <mishraaditya760@gmail.com>
2024-02-26 23:30:32 +0530

    Update Valid_perfect_square.java

--- Valid_perfect_square.java
+++ Valid_perfect_square.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean isPerfectSquare(int num) {
-        int low =1;
+        int low=1;
         int high=num;
 
         while(low<=high)
         {
-            long mid=low + (high-low)/2;
+            long mid= low+(high-low)/2;
 
             if(mid*mid==num)
             return true;
