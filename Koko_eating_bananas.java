4b4624d102ed02e1cc1a16658a299dffe82a9096
Aditya Mishra <mishraaditya760@gmail.com>
2024-03-02 22:05:50 +0530

    Create Koko_eating_bananas.java

---
+++ Koko_eating_bananas.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        
+        int low=1;
+        int high=1000000000;
+        
+        int ans=0;
+
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+
+            if(possible(piles,mid,h))
+            {
+                ans=mid;
+                high=mid-1;
+            }
+            else {
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+    public boolean possible(int[] piles, int mid, int h)
+    {
+
+        long sum=0;
+
+        for(int i:piles) {
+        sum += i/mid;
+
+        if(i%mid!=0)
+        sum++;
+        }
+
+        if(sum>h)
+        return false;
+
+        return true;
+
+    }
+}
