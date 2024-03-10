2d5ee142a246e70116cfad764eacaa1a2f27e9d1
Aditya Mishra <mishraaditya760@gmail.com>
2024-03-10 21:55:23 +0530

    Update Partition_array_according_to_pivot.java

--- Partition_array_according_to_pivot.java
+++ Partition_array_according_to_pivot.java
@@ -12,8 +12,8 @@ class Solution {
         }
          for(int i=0; i<nums.length; i++)
         {
-            if(nums[i]==pivot)
-            arr[index++]=nums[i];
+            if(nums[i]==pivot)                                                                    T.C- O(3n)
+            arr[index++]=nums[i];                                                                 S.C- O(1)
         }
          for(int i=0; i<nums.length; i++)
         {
@@ -21,5 +21,37 @@ class Solution {
             arr[index++]=nums[i];
         }
     return arr;
+    }
+}
+........................................................................
+
+class Solution {
+    public int[] pivotArray(int[] nums, int pivot) {
+        
+        for(int i=0; i<nums.length; i++)
+        {                                                                                            T.C- O(n^2)
+            if(nums[i]<pivot)                                                                        S.C- O(1)
+            {
+                for(int j=i; j>0 && nums[j-1]>=pivot; j--)
+                {
+                    int temp=nums[j-1];
+                    nums[j-1]=nums[j];
+                    nums[j]=temp;
+                }
+            }
+        }
+        for(int i=0; i<nums.length; i++)
+        {
+            if(nums[i]==pivot)
+            {
+                for(int j=i; j>0 && nums[j-1]>pivot; j--)
+                {
+                    int temp=nums[j-1];
+                    nums[j-1]=nums[j];
+                    nums[j]=temp;
+                }
+            }
+        }
+        return nums;
     }
 }
