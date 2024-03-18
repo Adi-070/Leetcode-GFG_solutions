e7a66e8d5d9acf934c9526334b4b830dc48dbf44
Aditya Mishra <mishraaditya760@gmail.com>
2024-03-18 23:48:32 +0530

    Create Non_overlapping_intervals.java

---
+++ Non_overlapping_intervals.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        
+        Arrays.sort(intervals, (a,b) -> (a[0]-b[0]));
+
+        int end = intervals[0][1];
+
+        int count=1;
+
+        for(int i=1; i<intervals.length; i++)
+        {
+            if(intervals[i][0]>=end)
+            {
+                count++;
+                end=intervals[i][1];
+            }
+            else 
+            end = Math.min(end,intervals[i][1]);
+        }
+        return intervals.length-count;
+    }
+}
