28a1d189c0c05cbbc4e6754a9526977c265136cd
Aditya Mishra <mishraaditya760@gmail.com>
2024-03-22 23:33:47 +0530

    Create Find_words_containing_character.java

---
+++ Find_words_containing_character.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        
+        List<Integer> list = new ArrayList<>();
+
+        for(int i=0; i<words.length; i++)
+        {
+          for(int j=0; j<words[i].length(); j++)
+          {
+            if(words[i].charAt(j)==x)
+            {
+                list.add(i);
+                break;
+            }
+          }
+          
+        }
+        return list;
+    }
+}
