41d86bb1158ac451c48f29f41cf2528bfb768a2e
Aditya Mishra <mishraaditya760@gmail.com>
2024-03-23 19:04:34 +0530

    Create Largest_odd_number_in_string.java

---
+++ Largest_odd_number_in_string.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String largestOddNumber(String num) {
+        
+        int i=num.length()-1;
+
+        while(i>=0)
+        {
+            if(num.charAt(i)%2 !=0)
+            return num.substring(0,i+1);
+
+            i--;
+        }
+        return "";
+    }
+}
