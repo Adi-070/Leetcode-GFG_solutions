8e2fe7f376611691185cf68c86a48d578b20bc0c
Aditya Mishra <mishraaditya760@gmail.com>
2024-03-24 12:40:33 +0530

    Update Find_the_duplicate_number.java

--- Find_the_duplicate_number.java
+++ Find_the_duplicate_number.java
@@ -1,5 +1,34 @@
 class Solution {
     public int findDuplicate(int[] nums) {
+        
+        Arrays.sort(nums);
+
+        int low=1;
+        int high=nums.length-1;
+
+
+        while(low<=high)
+        {
+            int mid = low+(high-low)/2;
+            int count=0;
+           for(int i:nums)
+           {
+            if(i<=mid)
+            count++;
+           }
+           if(count>mid)
+           high=mid-1;
+
+           else
+           low=mid+1;
+
+        }
+        return low;
+    }
+}
+......................................................................
+class Solution {
+    public int findDuplicate(int[] nums) {
        HashSet<Integer> set = new HashSet<>();
 
        for(int i:nums)
