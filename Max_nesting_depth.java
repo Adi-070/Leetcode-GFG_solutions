c192db16d8ade92d46429b6f61d61857c03bebbc
Aditya Mishra <mishraaditya760@gmail.com>
2024-04-03 22:34:39 +0530

    Create Max_nesting_depth.java

---
+++ Max_nesting_depth.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxDepth(String s) {
+        int counter=0;
+        int max=0;
+
+        for(int i=0; i<s.length(); i++)
+        {
+            if(s.charAt(i)=='(')
+            counter++;
+
+            else if(s.charAt(i)==')')
+            counter--;
+
+            max=Math.max(counter,max);
+        }
+        return max;
+    }
+}
