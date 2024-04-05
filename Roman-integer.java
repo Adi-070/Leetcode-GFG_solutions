18fa5dab4c052dca50251755fae19e7341876b29
Aditya Mishra <mishraaditya760@gmail.com>
2024-04-05 22:00:29 +0530

    Update Roman-integer.java

--- Roman-integer.java
+++ Roman-integer.java
@@ -1,65 +1,25 @@
 class Solution {
     public int romanToInt(String s) {
-        int i, j;
-        
-        s += 'A';
-        
-        int num = 0;
-        
-        for(i = 0; i < s.length(); i++)
+        HashMap<Character,Integer> map = new HashMap<>();
+
+        map.put('I',1);
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
+
+        int res=map.get(s.charAt(s.length()-1));
+
+        for(int i=s.length()-2; i>=0; i--)
         {
-            if(s.charAt(i) == 'I')
-            {
-                if(s.charAt(i+1) == 'X' || s.charAt(i+1) == 'V')
-                {
-                    num -= 1;
-                }
-                else
-                {
-                    num += 1;
-                }
-            }
-            else if(s.charAt(i) == 'X')
-            {
-                if(s.charAt(i+1) == 'C' || s.charAt(i+1) == 'L')
-                {
-                    num -= 10;
-                }
-                else
-                {
-                    num += 10;
-                }
-            }
-            else if(s.charAt(i) == 'C')
-            {
-                if(s.charAt(i+1) == 'D' || s.charAt(i+1) == 'M')
-                {
-                    num -= 100;
-                }
-                else
-                {
-                    num += 100;
-                }
-            }
-            else if(s.charAt(i) == 'V')
-            {
-                num += 5;
-            }
-            else if(s.charAt(i) == 'L')
-            {
-                num += 50;
-            }
-            else if(s.charAt(i) == 'D')
-            {
-                num += 500;
-            }
-            else if(s.charAt(i) == 'M')
-            {
-                num += 1000;
-            }
-            
-            System.out.println("sum : " + num);
+            if(map.get(s.charAt(i))<map.get(s.charAt(i+1)))
+            res -= map.get(s.charAt(i));
+
+            else
+            res += map.get(s.charAt(i));
         }
-        return num;
+        return res;
     }
 }
