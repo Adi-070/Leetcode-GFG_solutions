d999c9cdc173d40811907d33eb9fd4dd00361ce2
Aditya Mishra <mishraaditya760@gmail.com>
2024-04-09 16:57:59 +0530

    Update Time_needed_to_buy_tickets.java

--- Time_needed_to_buy_tickets.java
+++ Time_needed_to_buy_tickets.java
@@ -13,3 +13,32 @@ class Solution {
         return c;
     }
 }
+...................................................................
+
+class Solution {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        Queue<Integer> q = new LinkedList<>();
+        int c=0;
+
+        for(int i=0; i<tickets.length; i++)
+        q.add(i);
+
+        while(!q.isEmpty())
+        {
+            int front = q.remove();
+            tickets[front]--;
+
+            c++;
+
+            if(tickets[front]==0 && front==k)
+            break;
+
+            if(tickets[front]>0)
+            q.add(front);
+
+            else
+            continue;
+        }
+        return c;
+    }
+}
