b20d27635da346da3057d789fd5bec1e9a1be00d
Aditya Mishra <mishraaditya760@gmail.com>
2024-04-13 21:58:37 +0530

    Update Min_stack.java

--- Min_stack.java
+++ Min_stack.java
@@ -39,3 +39,55 @@ class MinStack {
  * int param_3 = obj.top();
  * int param_4 = obj.getMin();
  */
+..............................................................................
+
+class MinStack {
+
+    static class Pair
+    {   int a;
+        int b;
+        Pair(int a, int b)
+        {
+        this.a=a;
+        this.b=b;
+        }
+    }
+    Stack<Pair> stk ;
+
+    public MinStack() {
+        stk = new Stack<>();
+    }
+    
+    public void push(int val) {
+        int min=Integer.MAX_VALUE;
+        if(stk.isEmpty())
+        min = val;
+
+        else
+        min = Math.min(stk.peek().b, val);
+
+        stk.push(new Pair(val,min));
+
+    }
+    
+    public void pop() {
+        stk.pop();
+    }
+    
+    public int top() {
+        return stk.peek().a;
+    }
+    
+    public int getMin() {
+        return stk.peek().b;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
