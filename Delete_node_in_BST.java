d333563c7b3e7ccdfd74edec28bcdd827c06b0fb
Aditya Mishra <mishraaditya760@gmail.com>
2024-11-08 17:05:39 +0530

    Update Delete_node_in_BST.java

--- Delete_node_in_BST.java
+++ Delete_node_in_BST.java
@@ -13,6 +13,82 @@
  *     }
  * }
  */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+   
+
+        if(root==null)
+        return null;
+
+        if(root.val==key)
+        return helper(root);
+
+     TreeNode temp = root;
+
+        while(root!=null)
+        {
+            if(key<root.val)
+            {
+                if(root.left!=null && root.left.val==key)
+                {
+                    root.left=helper(root.left);
+                    break;
+                }
+                else{
+                    root=root.left;
+                }
+            }
+            else{
+                 if(root.right!=null && root.right.val==key)
+                {
+                    root.right=helper(root.right);
+                    break;
+                }
+                else{
+                    root=root.right;
+                }
+            }
+        }
+        return temp;
+    }
+    public TreeNode helper(TreeNode root)
+    {
+        if(root.left==null)
+        return root.right;
+
+        else if(root.right==null)
+        return root.left;
+
+        TreeNode rightChild = root.right;
+        TreeNode lastRight = findLastRight(root.left);
+        lastRight.right=rightChild;
+        return root.left;
+    }
+    public TreeNode findLastRight(TreeNode root)
+    {
+        if(root.right==null)
+        return root;
+
+        return findLastRight(root.right);
+    }
+}
+............................................................................................
 class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
         if(root==null)
