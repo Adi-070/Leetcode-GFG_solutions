4d6a889011e397a7284079908e34a3365a8f2550
Aditya Mishra <mishraaditya760@gmail.com>
2024-11-11 11:49:07 +0530

    Update LCA_BST.java

--- LCA_BST.java
+++ LCA_BST.java
@@ -10,16 +10,16 @@
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root==p || root==q)
+        if(root==p || root==q)
         return root;
+
         if(p.val<root.val && q.val>root.val)
         return root;
-        if(p.val<root.val && q.val<root.val)
-        return lowestCommonAncestor(root.left, p, q);
-        if(p.val>root.val && q.val>root.val)
-        return lowestCommonAncestor(root.right, p, q);
+        else if(p.val<root.val && q.val<root.val)
+        return lowestCommonAncestor(root.left,p,q);
+        else if(p.val>root.val && q.val>root.val)
+        return lowestCommonAncestor(root.right,p,q);
 
         return root;
-
     }
 }
