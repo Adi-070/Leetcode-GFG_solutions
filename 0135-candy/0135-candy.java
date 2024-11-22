d8f4ef70631cb59bc76bfd0a2c304ca3d3e40279
Aditya Mishra <mishraaditya760@gmail.com>
2024-11-22 15:47:25 +0530

    Time: 3 ms (83.16%), Space: 45.7 MB (37.84%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        if (n == 0) return 0;
+
+        int[] candy = new int[n];
+
+     
+        for (int i = 0; i < n; i++) {
+            candy[i] = 1;
+        }
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candy[i] = candy[i - 1] + 1;
+            }
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candy[i] = Math.max(candy[i], candy[i + 1] + 1);
+            }
+        }
+
+
+        int totalCandies = 0;
+        for (int c : candy) {
+            totalCandies += c;
+        }
+
+        return totalCandies;
+    }
+}
