5702bfb522b36810552be75c655fe6797aef59ab
Aditya Mishra <mishraaditya760@gmail.com>
2024-12-01 19:39:29 +0530

    Time: 3 ms (73.25%), Space: 48.2 MB (32.58%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -2,7 +2,7 @@ class Solution {
     public List<Integer> majorityElement(int[] nums) {
         int count1=0;
         int count2=0;
-
+//Moore's voting algo but we need 2 elements here due to n/3 restrain. So we follow the traditional way 
         int num1=Integer.MIN_VALUE;
         int num2=Integer.MIN_VALUE;
 
@@ -10,13 +10,13 @@ class Solution {
 
         for(int i=0; i<nums.length; i++)
         {
-            if(count1==0 && nums[i]!=num2)
+            if(count1==0 && nums[i]!=num2) //second cond is edge case to make sure the candidate element in num2 is not already present in num1
             {
                 count1=1;
                 num1=nums[i];
             }
 
-            else if(count2==0 && nums[i]!=num1)
+            else if(count2==0 && nums[i]!=num1)//second cond is edge case to make sure the candidate element in num1 is not already present in num2
             {
                 num2=nums[i];
                 count2=1;
